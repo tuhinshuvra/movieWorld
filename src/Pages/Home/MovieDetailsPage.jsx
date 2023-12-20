72bc0b9ef7082055758dba4d44ac24c4e31d90ff
Tuhin <tuhinshuvra18@gmail.com>
2023-12-21 00:55:50 +0600

    added some update

--- src/Pages/Home/MovieDetailsPage.jsx
+++ src/Pages/Home/MovieDetailsPage.jsx
@@ -1,11 +1,11 @@
 import './MovieDetailsPage.css';
-// import homePageImg from '../../../assets/movieImages/Home.jpg'
+import MovieDetails from '../../assets/movieImages/Movie Details.jpg'
 
 const MovieDetailsPage = () => {
     return (
         <div>
             <h2>This is movie Details  page</h2>
-            {/* <img src={homePageImg} className=' img-fluid' alt="" /> */}
+            <img src={MovieDetails} className=' img-fluid' alt="" />
         </div>
     );
 };
